33b0b40cec52776fe5f5ac0dcc884c6c619fc304
OhRev <121680448+OhRev@users.noreply.github.com>
2023-01-24 14:22:44 +0100

    Add Test

---
+++ src/test/java/org/example/SampleClassTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleClassTest {
+    @Test
+    sampleTest(){
+        final int CHOSEN_VALUE = 5;
+        SampleClass sampleClass = new SampleClass(CHOSEN_VALUE);
+        assertEquals (CHOSEN_VALUE, sampleClass.getNumber());
+
+    }
+
+}
